a409359af9ffac7879cd7356e240b884c9573994
Victor Silva <silvavictorda42@gmail.com>
2024-04-29 06:41:12 +0100

    removendo extra paragraphs from carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -327,8 +327,8 @@
                         <div class="container">
                           <div class="carousel-caption text-start">
                             <h1>Example headline.</h1>
-                            <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                        
+                            <p><a class="btn btn-lg btn-light" href="#">Sign up today</a></p>
                           </div>
                         </div>
                       </div>
@@ -336,9 +336,9 @@
                         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
                         <div class="container">
                           <div class="carousel-caption">
-                            <h1>Another example headline.</h1>
-                            <p>Some representative placeholder content for the second slide of the carousel.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                            <h1>A NEW WORLD IS BORN</h1>
+                            
+                            <p><a class="btn btn-lg btn-light" href="#">Learn more</a></p>
                           </div>
                         </div>
                       </div>
@@ -347,8 +347,8 @@
                         <div class="container">
                           <div class="carousel-caption text-end">
                             <h1>One more for good measure.</h1>
-                            <p>Some representative placeholder content for the third slide of this carousel.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                            
+                            <p><a class="btn btn-lg btn-light" href="#">Browse gallery</a></p>
                           </div>
                         </div>
                       </div>
