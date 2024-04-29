6c409f5592f5c5532371248376133a36f5c116f3
Victor Silva <silvavictorda42@gmail.com>
2024-04-29 08:20:47 +0100

    actualizando codigo das listas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -391,58 +391,30 @@
                     
                   </div>
                   <h2 class="h2">TOP 3 - Statistics of last 7 days in this platform </h2>
-                  <div class="row mb-2">
-                    <div class="col-md-4">
-                      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                          <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-                          <h3 class="mb-0">Featured post</h3>
-                          <div class="mb-1 text-body-secondary">Nov 12</div>
-                          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-                          </a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                        </div>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-4">
+                        <img src="..." class="rounded float-start" alt="...">
+                      </div>
+                      <div class="col-lg-4">
+                        <ul class="list-group">
+                          <li class="list-group-item">An item</li>
+                          <li class="list-group-item">A second item</li>
+                          <li class="list-group-item">A third item</li>
+                          <li class="list-group-item">A fourth item</li>
+                          <li class="list-group-item">And a fifth one</li>
+                        </ul>
+                      </div>
+                      <div class="col-lg-4">
+                        <ul class="list-group">
+                          <li class="list-group-item">An item</li>
+                          <li class="list-group-item">A second item</li>
+                          <li class="list-group-item">A third item</li>
+                          <li class="list-group-item">A fourth item</li>
+                          <li class="list-group-item">And a fifth one</li>
+                        </ul>
                       </div>
                     </div>
-                    <div class="col-md-4">
-                      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                          <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                          <h3 class="mb-0">Post title</h3>
-                          <div class="mb-1 text-body-secondary">Nov 11</div>
-                          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-                          </a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                          <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                            <h3 class="mb-0">Post title</h3>
-                            <div class="mb-1 text-body-secondary">Nov 11</div>
-                            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                              Continue reading
-                              <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
-                            </a>
-                          </div>
-                          <div class="col-auto d-none d-lg-block">
-                            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                          </div>
-                        </div>
-                      </div>
                   </div>
                 </main>
             <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
